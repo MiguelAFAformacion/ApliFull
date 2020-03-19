273fe9f2bb6c88461f7e0830265e6ae2cc91ac60
Miguel Otero <miguel.afaformacion@gmail.com>
2020-03-19 18:10:48 +0100

    Añadido inicial de archivos de ApliFull

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserFormRequest;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Mostrar una lista de los registros
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        if ($request) {
+            $query = trim($request->get('search'));
+            $users = User::where('name', 'LIKE', '%' . $query . '%')
+                    ->orderBy('id', 'asc')
+                    ->paginate(5);
+                    //->simplePaginate(5);
+
+            return view('usuarios.index', ['users' => $users, 'search' => $query]);
+
+        }
+
+
+        //$users = User::all();
+
+        //return view('usuarios.index', ['users' => $users]);
+    }
+
+    /**
+     * Mostrar el formulario para crear un nuevo registro
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('usuarios.create');
+    }
+
+    /**
+     * Almacena los registros recién creados de create en la base de datos
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario = new User();
+
+        $usuario->name = request('name');
+        $usuario->email = request('email');
+        $usuario->password = request('password');
+
+        $usuario->save();
+
+        return redirect('/usuarios');
+    }
+
+    /**
+     * Mostramos un registro específico
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('usuarios.show', ['user' => User::findOrFail($id)]);
+    }
+
+    /**
+     * Muestra un formulario con los datos a editar de un registro específico
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('usuarios.edit', ['user' => User::findOrFail($id)]);
+    }
+
+    /**
+     * Actualizar un registro en la base de datos
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UserFormRequest $request, $id)
+    {
+        $usuario = User::findOrFail($id);
+
+        $usuario->name = $request->get('name');
+        $usuario->email = $request->get('email');
+
+         $usuario->update();
+
+         return redirect('/usuarios');
+    }
+
+    /**
+     * Elimina un registro específico de la base de datos
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $usuario = User::findOrFail($id);
+
+        $usuario->delete();
+
+        return redirect('/usuarios');
+    }
+}
---
+++ resources/views/usuarios/index.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<h2>Lista de usuarios registrados <a href="usuarios/create"><button type="button" class="btn btn-success float-right">Agregar usuario</button></a></h2>
+	<h6>
+		@if($search)
+			<div class="alert alert-primary" role="alert">
+			  Los resultados para tu búsqueda '{{ $search}}' son:
+			</div>
+		@endif
+	</h6>
+	<table class="table table-hover">
+	  <thead>
+	    <tr>
+	      <th scope="col">ID</th>
+	      <th scope="col">nombre</th>
+	      <th scope="col">email</th>
+	      <th scope="col">opciones</th>
+	    </tr>
+	  </thead>
+	  <tbody>
+	  	@foreach ($users as $user)
+		    <tr>
+		      <th scope="row">{{$user->id}}</th>
+		      <td>{{$user->name}}</td>
+		      <td>{{$user->email}}</td>
+		      <td>
+		      	<form action="{{ route('usuarios.destroy', $user->id) }}" method="POST">
+		      		@csrf
+		      		@method('DELETE')
+		      		<a href="{{ route('usuarios.show', $user->id) }}"><button type="button" class="btn btn-secondary">Ver</button></a>
+		      		<a href="{{ route('usuarios.edit', $user->id) }}"><button type="button" class="btn btn-primary">Editar</button></a>
+		      		<button type="submit" class="btn btn-danger">Eliminar</button>
+		      	</form> 
+		     </td>
+		    </tr>
+	    @endforeach
+	  </tbody>
+	</table>
+	 <div class="row">
+	 	<div class="mx-auto">
+	 		{{ $users->links() }}
+	 	</div>
+	 </div>
+</div>
+@endsection
